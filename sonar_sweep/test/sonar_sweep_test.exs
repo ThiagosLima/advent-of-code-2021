607c1edac70e1777116340cdf177647b1f120020
Thiago Lima <thiagolima.unb@gmail.com>
2021-12-01 09:04:14 -0300

    test: create function sonar

--- sonar_sweep/test/sonar_sweep_test.exs
+++ sonar_sweep/test/sonar_sweep_test.exs
@@ -2,7 +2,15 @@ defmodule SonarSweepTest do
   use ExUnit.Case
   doctest SonarSweep
 
-  test "greets the world" do
-    assert SonarSweep.hello() == :world
+  test "the are no increase if there are no measures" do
+    assert SonarSweep.increase([]) == 0
+  end
+
+  test "the are no increase" do
+    assert SonarSweep.increase([1, 1, 1]) == 0
+  end
+
+  test "the are two increases" do
+    assert SonarSweep.increase([0, 1, 2]) == 2
   end
 end

ae539bdf2ceefe0d33d722c7c424de1ccdb3c4c3
Thiago Lima <thiagolima.unb@gmail.com>
2021-12-02 08:39:30 -0300

    test: add move function

--- dive/test/dive_test.exs
+++ dive/test/dive_test.exs
@@ -2,7 +2,18 @@ defmodule DiveTest do
   use ExUnit.Case
   doctest Dive
 
-  test "greets the world" do
-    assert Dive.hello() == :world
+  test "don't move if there is no course" do
+    assert Dive.move([]) == %{horizontal: 0, depth: 0}
+  end
+
+  test "go to the right position" do
+    assert Dive.move([
+             %{direction: "forward", quantity: 5},
+             %{direction: "down", quantity: 5},
+             %{direction: "forward", quantity: 8},
+             %{direction: "up", quantity: 3},
+             %{direction: "down", quantity: 8},
+             %{direction: "forward", quantity: 2}
+           ]) == %{horizontal: 15, depth: 10}
   end
 end

42ef0ed17adbf32b625085095bed9030e207e88f
Thiago Lima <thiagolima.unb@gmail.com>
2021-12-02 08:50:34 -0300

    test: add parser

--- dive/test/dive_test.exs
+++ dive/test/dive_test.exs
@@ -16,4 +16,19 @@ defmodule DiveTest do
              %{direction: "forward", quantity: 2}
            ]) == %{horizontal: 15, depth: 10}
   end
+
+  test "parse the input" do
+    input = "forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2"
+
+    parsedInput = [
+      %{direction: "forward", quantity: 5},
+      %{direction: "down", quantity: 5},
+      %{direction: "forward", quantity: 8},
+      %{direction: "up", quantity: 3},
+      %{direction: "down", quantity: 8},
+      %{direction: "forward", quantity: 2}
+    ]
+
+    assert Dive.parse(input) == parsedInput
+  end
 end

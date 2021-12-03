12e5394423a3b0269e2e2f18767e048c7ad96703
Thiago Lima <thiagolima.unb@gmail.com>
2021-12-03 08:59:45 -0300

    test: add parse function

--- binary_diagnostic/test/binary_diagnostic_test.exs
+++ binary_diagnostic/test/binary_diagnostic_test.exs
@@ -2,7 +2,22 @@ defmodule BinaryDiagnosticTest do
   use ExUnit.Case
   doctest BinaryDiagnostic
 
-  test "greets the world" do
-    assert BinaryDiagnostic.hello() == :world
+  test "parser" do
+    input = "00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010"
+
+    assert BinaryDiagnostic.parse(input) == [
+             [0, 0, 1, 0, 0],
+             [1, 1, 1, 1, 0],
+             [1, 0, 1, 1, 0],
+             [1, 0, 1, 1, 1],
+             [1, 0, 1, 0, 1],
+             [0, 1, 1, 1, 1],
+             [0, 0, 1, 1, 1],
+             [1, 1, 1, 0, 0],
+             [1, 0, 0, 0, 0],
+             [1, 1, 0, 0, 1],
+             [0, 0, 0, 1, 0],
+             [0, 1, 0, 1, 0]
+           ]
   end
 end

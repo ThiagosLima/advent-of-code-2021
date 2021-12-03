a02285383906ec5f9404ce7f60d645f5b8d76b5b
Thiago Lima <thiagolima.unb@gmail.com>
2021-12-03 08:25:58 -0300

    chore: create binary diagnostic project

---
+++ binary_diagnostic/lib/binary_diagnostic.ex
@@ -0,0 +1,18 @@
+defmodule BinaryDiagnostic do
+  @moduledoc """
+  Documentation for `BinaryDiagnostic`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BinaryDiagnostic.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ binary_diagnostic/test/binary_diagnostic_test.exs
@@ -0,0 +1,8 @@
+defmodule BinaryDiagnosticTest do
+  use ExUnit.Case
+  doctest BinaryDiagnostic
+
+  test "greets the world" do
+    assert BinaryDiagnostic.hello() == :world
+  end
+end

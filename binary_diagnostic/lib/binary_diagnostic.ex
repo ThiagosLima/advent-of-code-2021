0a039c75c6599534757cc6d28ce9445976652da3
Thiago Lima <thiagolima.unb@gmail.com>
2021-12-03 09:38:49 -0300

    feat: add power_comsumption function

--- binary_diagnostic/lib/binary_diagnostic.ex
+++ binary_diagnostic/lib/binary_diagnostic.ex
@@ -3,6 +3,26 @@ defmodule BinaryDiagnostic do
   Documentation for `BinaryDiagnostic`.
   """
 
+  def power_consumption(report) do
+    most_common_bit =
+      report
+      |> Enum.map(fn d -> Enum.sum(d) > Enum.count(d) / 2 end)
+
+    gama =
+      most_common_bit
+      |> Enum.map(&if &1, do: "1", else: "0")
+      |> Enum.join("")
+      |> String.to_integer(2)
+
+    epsilon =
+      most_common_bit
+      |> Enum.map(&if &1, do: "0", else: "1")
+      |> Enum.join("")
+      |> String.to_integer(2)
+
+    gama * epsilon
+  end
+
   def parse(input) do
     input
     |> String.split("\n")
@@ -11,5 +31,7 @@ defmodule BinaryDiagnostic do
       |> String.codepoints()
       |> Enum.map(&String.to_integer/1)
     end)
+    |> List.zip()
+    |> Enum.map(&Tuple.to_list/1)
   end
 end

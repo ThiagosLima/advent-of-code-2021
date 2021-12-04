68607d45cdd4bf4302cd6437198355872fc9fac8
Thiago Lima <thiagolima.unb@gmail.com>
2021-12-04 08:45:30 -0300

    test: winning_row function

--- giant_squid/test/giant_squid_test.exs
+++ giant_squid/test/giant_squid_test.exs
@@ -2,7 +2,27 @@ defmodule GiantSquidTest do
   use ExUnit.Case
   doctest GiantSquid
 
-  test "greets the world" do
-    assert GiantSquid.hello() == :world
+  test "there is a winning row" do
+    matrix = [
+      [nil, nil, nil, nil, nil],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0]
+    ]
+
+    assert GiantSquid.winning_row(matrix) == true
+  end
+
+  test "there is not a winning row" do
+    matrix = [
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0]
+    ]
+
+    assert GiantSquid.winning_row(matrix) == false
   end
 end

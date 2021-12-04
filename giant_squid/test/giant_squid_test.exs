25cf7e9183363795acaaf90b7c4903d3ed9f5119
Thiago Lima <thiagolima.unb@gmail.com>
2021-12-04 08:52:09 -0300

    test: winning_col function

--- giant_squid/test/giant_squid_test.exs
+++ giant_squid/test/giant_squid_test.exs
@@ -25,4 +25,28 @@ defmodule GiantSquidTest do
 
     assert GiantSquid.winning_row(matrix) == false
   end
+
+  test "there is a winning col" do
+    matrix = [
+      [nil, 0, 0, 0, 0],
+      [nil, 0, 0, 0, 0],
+      [nil, 0, 0, 0, 0],
+      [nil, 0, 0, 0, 0],
+      [nil, 0, 0, 0, 0]
+    ]
+
+    assert GiantSquid.winning_col(matrix) == true
+  end
+
+  test "there is not a winning col" do
+    matrix = [
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0]
+    ]
+
+    assert GiantSquid.winning_col(matrix) == false
+  end
 end

bccbb12bf1f45dbb4b3a24a7400df626de9e1e33
Thiago Lima <thiagolima.unb@gmail.com>
2021-12-04 08:51:34 -0300

    feat: add winning_col function

--- giant_squid/lib/giant_squid.ex
+++ giant_squid/lib/giant_squid.ex
@@ -6,4 +6,16 @@ defmodule GiantSquid do
   def winning_row(matrix) do
     Enum.count(matrix, fn row -> Enum.filter(row, &(&1 != nil)) != [] end) != 5
   end
+
+  def winning_col(matrix) do
+    matrix
+    |> transpose
+    |> winning_row
+  end
+
+  defp transpose(matrix) do
+    matrix
+    |> Enum.zip
+    |> Enum.map(&Tuple.to_list/1)
+  end
 end

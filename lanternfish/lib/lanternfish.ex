7ab04c9776ff127754fff19f6a174c1f6c223c1c
Thiago Lima <thiagolima.unb@gmail.com>
2021-12-06 15:42:03 -0300

    feat: add part one

--- lanternfish/lib/lanternfish.ex
+++ lanternfish/lib/lanternfish.ex
@@ -3,9 +3,9 @@ defmodule Lanternfish do
   Documentation for `Lanternfish`.
   """
 
-  def fishes_after_days(days, initial) do
+  def fishes_after_days(initial, days) do
     Enum.reduce(1..days//1, initial, fn _day, fishes -> multiply(fishes) end)
-    |> Enum.count
+    |> Enum.count()
   end
 
   defp multiply(fishes) do
@@ -16,5 +16,19 @@ defmodule Lanternfish do
       if fish == 0, do: 6, else: fish - 1
     end)
     |> Enum.concat(Enum.take(Stream.cycle([8]), zeros))
+  end
+
+  defp parse(input) do
+    input
+    |> String.split(",")
+    |> Stream.map(&String.to_integer/1)
+  end
+
+  def part_one do
+    {:ok, contents} = File.read("./lib/input.txt")
+
+    contents
+    |> parse
+    |> fishes_after_days(80)
   end
 end
--- lanternfish/test/lanternfish_test.exs
+++ lanternfish/test/lanternfish_test.exs
@@ -3,20 +3,20 @@ defmodule LanternfishTest do
   doctest Lanternfish
 
   test "fihes after 0 days" do
-    initial = [3,4,3,1,2]
+    initial = [3, 4, 3, 1, 2]
 
-    assert Lanternfish.fishes_after_days(0, initial) == 5
+    assert Lanternfish.fishes_after_days(initial, 0) == 5
   end
 
   test "fihes after 18 days" do
-    initial = [3,4,3,1,2]
+    initial = [3, 4, 3, 1, 2]
 
-    assert Lanternfish.fishes_after_days(18, initial) == 26
+    assert Lanternfish.fishes_after_days(initial, 18) == 26
   end
 
   test "fihes after 80 days" do
-    initial = [3,4,3,1,2]
+    initial = [3, 4, 3, 1, 2]
 
-    assert Lanternfish.fishes_after_days(80, initial) == 5934
+    assert Lanternfish.fishes_after_days(initial, 80) == 5934
   end
 end

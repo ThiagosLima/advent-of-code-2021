4a5c484caefbee542ece5cf3863a7de4c7f34d46
Thiago Lima <thiagolima.unb@gmail.com>
2021-12-06 15:35:55 -0300

    test: add fishes_after_days function

--- lanternfish/test/lanternfish_test.exs
+++ lanternfish/test/lanternfish_test.exs
@@ -2,7 +2,21 @@ defmodule LanternfishTest do
   use ExUnit.Case
   doctest Lanternfish
 
-  test "greets the world" do
-    assert Lanternfish.hello() == :world
+  test "fihes after 0 days" do
+    initial = [3,4,3,1,2]
+
+    assert Lanternfish.fishes_after_days(0, initial) == 5
+  end
+
+  test "fihes after 18 days" do
+    initial = [3,4,3,1,2]
+
+    assert Lanternfish.fishes_after_days(18, initial) == 26
+  end
+
+  test "fihes after 80 days" do
+    initial = [3,4,3,1,2]
+
+    assert Lanternfish.fishes_after_days(80, initial) == 5934
   end
 end
